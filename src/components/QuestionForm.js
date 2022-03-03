3aca468713c9571040ca152b888b23e51abb2998
Olivia Pomeroy <opomeroy26@gmail.com>
2022-03-02 18:09:10 -0800

    commit tree 13d4af35a39eb736f0d4bb00343bf107d4789791

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -20,7 +20,26 @@ function QuestionForm(props) {
   function handleSubmit(event) {
     event.preventDefault();
     console.log(formData);
-  }
+
+
+    const questionData = {
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4, ],
+      correctIndex: formData.correctIndex
+    };
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(questionData),
+    })
+    .then((r) => r.json())
+}
 
   return (
     <section>
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
-  return (
+  const [questions, setQuestions] = useState([])
+
+  useEffect(()=> {
+    fetch("http://localhost:4000/questions")
+    .then((resp)=> resp.json())
+    .then((dataArr) => setQuestions(dataArr));
+      
+    }, [])
+
+    function handleDeleteClick(id){
+      console.log("clicked delete")
+      fetch(`http://localhost:4000/questions/${id}`, {
+      method: "Delete",
+      }) 
+      .then((resp)=> resp.json())
+      .then(()=> {
+        const updatedQuestionList = questions.filter((question)=>question.id !==  id);
+      setQuestions(updatedQuestionList)
+    });
+  }
+
+    const dataToDisplay = questions.map((questions) => 
+    <QuestionItem 
+      question= {questions}
+      onDeleteClick = {handleDeleteClick}
+    /> )
+
+    return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul> {dataToDisplay}</ul>
     </section>
   );
 }
